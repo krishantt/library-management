6bac7fb5571438e701ecfbc940993fb5c04adf97
krishantt <154585072+krishantt@users.noreply.github.com>
2024-02-02 03:28:21 +0545

    Trying auth

--- utils/models.py
+++ utils/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from passlib.context import CryptContext
 
 Base = declarative_base()
 
